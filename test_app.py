1511740f0ec2af0b5457687787ab9b95e83ab9d8
apiramyjeyaharan <apiramyjeyaharan@gmail.com>
2026-03-23 22:31:09 +0530

    name added

--- test_app.py
+++ test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_home():
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"Hello, World!"
+    assert response.data == b"Hello, World! Apiramy"
